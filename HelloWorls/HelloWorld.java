90bc912a729f03c6080db11811c8921bc404bd21
gizem <114784169+gizemokatan@users.noreply.github.com>
2022-10-03 20:20:59 -0400

    Update HelloWorld.java

--- HelloWorls/HelloWorld.java
+++ HelloWorls/HelloWorld.java
@@ -3,8 +3,12 @@ package HelloWorls;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World2");
-        System.out.println("Hello World3");
+        /*
+        some
+        codes
+        for 
+        loginPage
+        */
         
     }
 }
